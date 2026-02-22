16d47f8913ee3dfe4e88b2330168f628dc4541e2
sooyoung159 <sooyoung159@naver.com>
2026-02-22 20:52:42 +0900

    AdSense 스크립트를 직접 script 태그로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -27,11 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6835019974856590"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
       </head>
       <body
